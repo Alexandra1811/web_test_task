f1057b50fb4f47b27b6c47473bfd5d3a46abd085
Alexandra1811 <krivonosova.alex@yandex.ru>
2018-04-02 16:03:38 +0300

    Update main.js

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,12 +1,12 @@
-$(document).on('click', '.str_block', function(){
-    $(this).removeClass( "str_block" ).addClass( "str_up_block" );
-    $('.ti_active').removeClass( "ti_active" );
-    $(this).parent().addClass( "ti_active" );
+$(document).on('click', '.thumbnail', function(){
+    var ti = $(this).find('.thumbnail_inner').parent();
+    if (ti.hasClass('ti_active')){
+        $(this).find('.str_block').removeClass( "str_up_block" ).addClass( "str_block" );
+        ti.removeClass( "ti_active" );
+    } else {
+        $(this).find('.str_block').removeClass( "str_block" ).addClass( "str_up_block" );
+        $('.ti_active').removeClass( "ti_active" );
+        ti.addClass( "ti_active" );
+    }
+
 });
-$(document).on('click', '.str_up_block', function(){
-    $(this).removeClass( "str_up_block" ).addClass( "str_block" );
-    $(this).parent().removeClass( "ti_active" );
-});
-
-
-
